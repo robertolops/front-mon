bdf587ffd6a3fb1bf7b74a07b3faddbb7a238c36
val7lima <73365892+val7lima@users.noreply.github.com>
2020-11-06 11:50:35 -0300

    Update usuario.js

--- js/usuario.js
+++ js/usuario.js
@@ -19,6 +19,26 @@ function CriaPDF() {
     win.print();                                                            // IMPRIME O CONTEUDO
 }
 
+function CriaPDF1() {
+    var minhaTabela = document.getElementById('resultado').innerHTML;
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 20px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+
+    // CRIA UM OBJETO WINDOW
+    var win = window.open('', '', 'height=700,width=800');
+    win.document.write('<html><head>');
+    win.document.write('<title> Relatório </title>');   // <title> CABEÇALHO DO PDF.
+    win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.write(minhaTabela);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
+    win.document.write('</body></html>');
+    win.document.close(); 	                                         // FECHA A JANELA
+    win.print();                                                            // IMPRIME O CONTEUDO
+}
 
 
 function gerarCSV(dados) {
